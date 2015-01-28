08e3cdeb21b7043b8f352b1685b7c172d58f279b
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2015-01-29 01:23:37 +0200

    new feature: re focus to input field after `enter` or send

--- client/assets/js/controllers/chat.js
+++ client/assets/js/controllers/chat.js
@@ -4,14 +4,37 @@
   angular.module('application')
   
 		.controller('ChatCtrl',function($scope,chat){
-			
-			$scope.message = '';
-			$scope.messages = chat.messages;
-			
-			$scope.send = function(){
-				chat.sendMessage($scope.message);
-				$scope.message = '';
-			};
+			 /**
+		     * Message input field id
+		     * @const {string}
+		     */
+		    var MESSAGE_INPUT_ID = "message_input";
+
+		    function send() {
+
+		        //do send
+		        chat.sendMessage($scope.message);
+
+		        clearFocus();
+		    }
+
+		    function clearFocus() {
+		        //set focus to input field
+		        document.getElementById(MESSAGE_INPUT_ID).focus();
+
+		        //clear message input
+		        $scope.message = "";
+		    }
+
+		 	function init(){
+		 		$scope.messages = chat.messages;
+		 		clearFocus();
+		 	}
+
+		 	init();
+
+		 	//Exports
+			$scope.send = send;
 			
 		});
 		
